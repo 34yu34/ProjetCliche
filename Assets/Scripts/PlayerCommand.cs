349e2dbd3939cb7023986e94a7991374e823bed2
Bill <billy.bouchard03@gmail.com>
2022-11-01 17:46:39 -0400

    Add Naughty Attributes

---
+++ Assets/Scripts/PlayerCommand.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Command", menuName = "Player/Command", order = 0)]
+public abstract class PlayerCommand : ScriptableObject
+{
+    
+    
+    public abstract void Execute(Player player);
+}
