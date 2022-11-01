8aa924ba7f3defb1990f05a398a109852dc7259d
Bill <billy.bouchard03@gmail.com>
2022-11-01 18:05:13 -0400

    Add Basic Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
-using UnityEngine.Serialization;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _movementSpeed;
+
+    [InputAxis]
+    [SerializeField] 
+    private string _horizontalInput;
+    
+    [InputAxis]
+    [SerializeField] 
+    private string _verticalInput;
+
+
+    private Rigidbody2D _rb;
     
     private void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        var movement = _movementSpeed * new Vector2(Input.GetAxis(_horizontalInput), Input.GetAxis(_verticalInput));
         
+        _rb.velocity = movement;
     }
 }
--- Assets/Scripts/PlayerCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(fileName = "Command", menuName = "Player/Command", order = 0)]
-public abstract class PlayerCommand : ScriptableObject
-{
-    
-    
-    public abstract void Execute(Player player);
-}
