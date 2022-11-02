af44d1159a3ba4f837cf1837618fd30c589072cd
Bill <billy.bouchard03@gmail.com>
2022-11-01 23:27:23 -0400

    Add directions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,42 +11,57 @@ public class Player : MonoBehaviour
     [SerializeField] private float _movementSpeed;
 
     [SerializeField] private float _viewDist;
-    
+
     private Rigidbody2D _rb;
     private PlayerInput _inputs;
 
     [SerializeField] private Interactable _currentInteractable;
+
+    private Vector2 _direction;
     
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _inputs = GetComponent<PlayerInput>();
+        _direction = Vector2.right;
     }
 
     public void FixedUpdate()
     {
         _rb.velocity = _inputs.MovementInput * _movementSpeed;
+
+        if (_inputs.MovementInput != Vector2.zero)
+        {
+            _direction = _inputs.MovementInput.normalized;
+        }
         
         LookForward();
     }
 
     private void LookForward()
     {
-        var position = transform.position;
-        RaycastHit2D hit = Physics2D.Raycast(new Vector2(position.x, position.y), _inputs.MovementInput, _viewDist, 1 << Interactable.InteractableLayer);
         
-        if (hit)
-        {
-            var interactable = hit.collider.GetComponent<Interactable>();
+        _currentInteractable = null;
+        
+        var position = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(position.x, position.y), _direction, _viewDist, 1 << Interactable.InteractableLayer);
+        
+        #if UNITY_EDITOR
+            Debug.DrawLine(position, position + (Vector3)_direction * _viewDist, Color.red);
+        #endif
 
-            if (interactable)
-            {
-                _currentInteractable = interactable;
-            }
-        }
-        else
+        if (!hit)
         {
-            _currentInteractable = null;
+            return;
         }
+
+        var interactable = hit.collider.GetComponent<Interactable>();
+
+        if (!interactable)
+        {
+            return;
+        }
+
+        _currentInteractable = interactable;
     }
 }
